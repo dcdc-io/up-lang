768973f3e12586499ca9f14dccebbbd267369e5b
Ben Babik <ben@dcdc.io>
2021-02-22 00:38:47 +0000

    wip

--- spec/parser.spec.ts
+++ spec/parser.spec.ts
@@ -5,11 +5,44 @@ const parse = code => (...rules:Parameters<ReturnType<typeof Tibu.parse>>) => Ti
 
 describe('parser', () => {
     it('can parse one line comments', () => {
-        parse("//")(
-            statements.comment.yields((r,y) => {
-                r.tokens[0] // ?
-                y // ?
-            })
+        const result = parse("// hello world")(
+            statements.comment
         )
+        expect(result).toStrictEqual([{
+            location: { index: 0 },
+            type: "comment",
+            raw: "// hello world",
+            value: "hello world"
+        }])
+    })
+
+    it('can parse import statements', () => {
+        const result = parse("import { foo } from 'bar'")(
+            statements.import
+        )
+        expect(result).toStrictEqual([{
+            location: { index: 0 },
+            type: "import",
+            raw: "import { foo } from 'bar'",
+            value: {
+                import: {
+                    location: { index: 7 },
+                    type: "destructuredNames",
+                    raw: "{ foo }",
+                    value: [{
+                        location: { index: 9 },
+                        type: "name",
+                        raw: "foo",
+                        value: "foo"
+                    }]
+                },
+                source: {
+                    location: { index: 20 },
+                    type: "string",
+                    raw: "'bar'",
+                    value: "bar"
+                }
+            }
+        }])
     })
 })
--- src/parser.ts
+++ src/parser.ts
@@ -1,18 +1,70 @@
-import { Tibu } from "tibu"
+import { Input, IRule, Result, Tibu } from "tibu"
 
 const { parse: tibu, rule, many, either, all, optional, token } = Tibu
+
+const utility = {
+    StartOfLine: (input: Input) => {
+        let result: Result;
+        const matches = input.source.match(/\r\n|\r|\n/gm)
+    }
+}
+
+const tokens = {
+    WhitespaceAny: token("WhitespaceAny", /\s*/),
+    WhitespaceAnyMultiline: token("WhitespaceAnyMultiline", /\s*/m),
+    UntilEndOfLine: token("UntilEndOfLine", /.*$/m),
+    CommentStart: token("CommentStart", "//"),
+    Import: token("Import", "import"),
+    From: token("From", "from"),
+    NameOrIdentifier: token("NameOrIdentifier", /[a-z_@$]+[a-z_@$0-9]*/)
+}
+
+type Comment = {
+
+}
 
 export const statements = {
     statement: rule(() => statements.comment),
     comment: rule(
-        () => rule(
-            token("comment", "//")
+        tokens.WhitespaceAnyMultiline,
+        tokens.CommentStart,
+        tokens.WhitespaceAny,
+        tokens.UntilEndOfLine
+    ).yields((r, y) => {
+        return {
+            location: { index: r.tokens[0].result.startloc },
+            type: "comment",
+            value: r.one("UntilEndOfLine"),
+            raw: r.tokens.map(t => t.result.value).join("")
+        }
+    }),
+    import: rule(
+        tokens.WhitespaceAnyMultiline,
+        tokens.Import,
+        tokens.WhitespaceAnyMultiline,
+        either(
+            () => statements.name,
+            () => statements.destructuredNames
+        ),
+        tokens.WhitespaceAnyMultiline,
+        tokens.From,
+        tokens.WhitespaceAnyMultiline,
+        () => statements.stringLiteral
+    ),
+    name: rule(
+        tokens.NameOrIdentifier
+    ),
+    stringLiteral: rule(
+        either(
+            rule(
+                tokens.QuoteSingle, tokens.
+            )
         )
     )
 }
 
-export const parse = (code: string) => {
-    tibu(code)(
-        rule(many(statement))
-    )
-}
+// export const parse = (code: string) => {
+//     tibu(code)(
+//         rule(many(statement))
+//     )
+// }
